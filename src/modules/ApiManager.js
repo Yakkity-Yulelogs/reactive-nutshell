d2540e71aa94e56d3dda734c6106fe50413f0bfa
Ryan Bishop <rdbishop06@gmail.com>
2019-12-02 15:55:54 -0600

    create API Manager for general reuse between components

---
+++ src/modules/ApiManager.js
@@ -0,0 +1,34 @@
+const baseUrl = 'http://localhost:5002';
+
+export default {
+	get(endpoint, id, params="") {
+		return fetch(`${baseUrl}/${endpoint}/${id}?${params}`).then((result) => result.json());
+	},
+	getAll(endpoint, params="") {
+		return fetch(`${baseUrl}/${endpoint}?${params}`).then((result) => result.json());
+	},
+	delete(endpoint, id) {
+		return fetch(`${baseUrl}/${endpoint}/${id}`, {
+			method: 'DELETE'
+		}).then((result) => result.json());
+    },
+    post(endpoint, newObject) {
+        return fetch(`${baseUrl}/${endpoint}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newObject)
+        }).then(data => data.json())
+    },
+    update(endpoint, editedObj) {
+        // make sure to pass in the ID in the 'editedObj' so you can access it in the fetch call
+        return fetch(`${baseUrl}/${endpoint}/${editedObj.id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(editedObj)
+        }).then(data => data.json());
+      }
+};
