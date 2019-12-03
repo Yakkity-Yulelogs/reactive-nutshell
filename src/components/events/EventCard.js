80c3d1161a63965662d41dd17493757f584f7641
Ryan Crowley <ryancrowleycode@gmail.com>
2019-12-03 09:44:28 -0600

    created EventCard.js and EventList.js. Began code for EventCard.js which currently renders an empty React Fragment.

---
+++ src/components/events/EventCard.js
@@ -0,0 +1,17 @@
+// (RC) This component is responsible for rendering a single event card.
+
+import React, { Component } from 'react'
+
+
+class EventCard extends Component {
+    render () {
+        return (
+            <React.Fragment>
+                
+            </React.Fragment>
+        )
+    }
+
+}
+
+export default EventCard
