b8686f8ca76cb913971e89e6a8d19cd361485bfc
Ryan Crowley <ryancrowleycode@gmail.com>
2019-12-03 14:13:37 -0600

    can see all events with own event on top

--- src/components/events/EventsApiManager.js
+++ src/components/events/EventsApiManager.js
@@ -3,6 +3,11 @@ import ApiManager from '../../modules/ApiManager'
 export default {
     getOwnEventsSorted(userId) {
         return ApiManager.getAll("events", `userId=${userId}&_sort=eventDate&_order=asc`)
-    }
+    },
+
+    getAllEventsSorted() {
+        return ApiManager.getAll("events", `_sort=eventDate&_order=asc`)
+    },
+
 
 }
--- src/components/events/EventsList.js
+++ src/components/events/EventsList.js
@@ -8,28 +8,57 @@ const loggedInUser = 1
 
 class EventsList extends Component {
     state = {
-        events: [],
-        friendsEvents: [],
+        events: []
+    }
 
+    putOwnFirstEventFirst = (eventsArray) => {
+        const oldArray = eventsArray
+        const finalArray = []
+        let foundFirstEvent = false
+
+        for (const evt of oldArray) {
+            if (!foundFirstEvent && evt.userId === loggedInUser) {
+                // add user's own first event as the first index position in finalArray
+                finalArray.unshift(evt)
+                foundFirstEvent = true
+
+            } else {
+                // if own event or friends evt
+                if (evt.userId)
+                    finalArray.push(evt)
+            }
+        }
+        return finalArray
+    }
+
+    // create a string to allow query for each friend
+    createStringOfFriends(friendsArray) {
+        let friendsParam = ""
+
+        for (friend of friendsArray) {
+            friendsParam += `&userId=${friend.userId}`
+        }
+        return friendsParam
     }
 
 
-
-    componentDidMount () {
-        EventApiManager.getOwnEventsSorted(loggedInUser)
-        .then(eventsList => {
-            this.setState({
-                events: eventsList
+    componentDidMount() {
+        
+        EventApiManager.getAllEventsSorted()
+            .then(eventsList => {
+                this.setState({
+                    events: this.putOwnFirstEventFirst(eventsList)
+                    // events: eventsList
+                })
             })
-        })
     }
 
     render() {
         return (
             <React.Fragment>
                 <div className="container-cards" id="events">
-                    {this.state.events.map(event => 
-                        <EventsCard 
+                    {this.state.events.map(event =>
+                        <EventsCard
                             key={event.id}
                             event={event}
                         />)}
