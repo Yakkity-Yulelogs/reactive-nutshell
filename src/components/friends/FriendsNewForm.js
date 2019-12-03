73ede38d11dba7bc3019d3086fa2eded5955de59
Ryan Bishop <rdbishop06@gmail.com>
2019-12-03 14:20:51 -0600

    merged master

--- src/components/friends/FriendsNewForm.js
+++ src/components/friends/FriendsNewForm.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
+import FriendsCard from './FriendsCard'
+import ApiFriends from './ApiFriends';
 
 export class FriendsNewForm extends Component {
+    state = {
+        
+    }
+
+    componentDidMount() {
+        ApiFriends.getAllFriendsWithNames()
+            .then(friends => {
+                const newState = { friends: []};
+                friends.forEach(friend => {
+                    newState.friends = [...newState.friends,{deleteId: friend.id, ...friend.user}]
+                })
+                this.setState(newState)
+            })
+    }
+    
     render() {
         return (
             <div>
                 <h1>Add New Friend</h1>
                 <ul>
-                    <li>Friend List</li>
+                    <FriendsCard />
                 </ul>
             </div>
         )
