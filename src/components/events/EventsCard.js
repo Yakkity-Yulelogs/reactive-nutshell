d398fcf65d6f13be9fe00d370bf3f45273028b07
Ryan Crowley <ryancrowleycode@gmail.com>
2019-12-04 09:58:02 -0600

    events will now only show if they are current, meaning they take place today or at a future date.

--- src/components/events/EventsCard.js
+++ src/components/events/EventsCard.js
@@ -8,15 +8,6 @@ import { convertDateTimeFromISO } from "../../modules/DateTime"
 
 class EventsCard extends Component {
     render() {
-        // need to move this to list so that it doesn't add to the list if event is before
-        
-        const now = new Date()
-        // const now = (new Date().getDay()) + 1
-        const eventToday = (convertDateTimeFromISO(this.props.event.eventDate).getDay()) + 1
-        console.log("Today: ", now)
-        console.log("eventToday: ", eventToday)
-        // console.log("before? ", now > eventToday)
-
         return (
             <React.Fragment>
                 <div className={`card 
--- src/components/events/EventsList.js
+++ src/components/events/EventsList.js
@@ -1,6 +1,12 @@
+/*
+    Purpose: Make fetch calls to database for events and friends to create a list of event objects.
+    This component renders the EventsCards component.
+    Author(s): Ryan Crowley
+*/
 import React, { Component } from 'react'
 import EventsCard from './EventsCard'
 import EventApiManager from './EventsApiManager'
+import { convertDateTimeFromISO } from "../../modules/DateTime"
 import './Events.css'
 
 // WILL NEED TO CHANGE THIS ONCE LOGIN IS FUNCTIONING
@@ -11,22 +17,46 @@ class EventsList extends Component {
         events: []
     }
 
+    // returns boolean of whether event takes place before today or not before today
+    isCurrentEvent = (eventObject) => {
+        const now = new Date()
+        const nowDay = (now.getDay()) + 1
+        const nowYear = now.getFullYear()
+        const nowMonth = (now.getMonth()) + 1
+
+        const eventFullDate = convertDateTimeFromISO(eventObject.eventDate)
+        const eventToday = (eventFullDate.getDay()) + 1
+        const eventMonth = (eventFullDate.getMonth()) + 1
+        const eventYear = eventFullDate.getFullYear()
+
+        if (eventYear >= nowYear && eventMonth >= nowMonth && eventToday >= nowDay) {
+            return true
+        } else {
+            return false
+        }
+
+    }
+
     // returns new array where loggedInUser's first event is always the first event
     putOwnFirstEventFirst = (eventsArray) => {
         const oldArray = eventsArray
         const finalArray = []
         let foundFirstEvent = false
 
+        // we only want to update finalArray if the event takes place today or later.
         for (const evt of oldArray) {
-            if (!foundFirstEvent && evt.userId === loggedInUser) {
-                // add user's own first event as the first index position in finalArray
-                finalArray.unshift(evt)
-                foundFirstEvent = true
+            if (this.isCurrentEvent(evt)) {
 
-            } else {
-                // if own event or friends evt
-                if (evt.userId)
+                if (!foundFirstEvent && evt.userId === loggedInUser) {
+                    // add user's own first event as the first index position in finalArray
+                    finalArray.unshift(evt)
+                    foundFirstEvent = true
+                    
+                } else {
+                    // if own event or friends evt
+                    if (evt.userId)
                     finalArray.push(evt)
+                }
             }
         }
         return finalArray
