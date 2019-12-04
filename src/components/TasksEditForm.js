6b2686459ad1595a1001e23161f41a89982a941d
Cherkesky <guy@cherkesky.com>
2019-12-04 14:08:14 -0600

    edit functionality completed

--- src/components/TasksEditForm.js
+++ src/components/TasksEditForm.js
@@ -1,11 +1,97 @@
 import React, { Component } from 'react'
+import ApiManager from '../modules/ApiManager'
+
+const loggedInUser = 1 // REPLACE WITH LOCALSTORAGE
 
 export class TasksEditForm extends Component {
+//******************************************************************************
+//STATE
+//******************************************************************************
+  state = {
+    task: "",
+    expectedCompletionDate: "",
+    loadingStatus: true,
+  };
+//******************************************************************************
+//Handle form FIELD CHANGE and UPDATE the STATE accordingly 
+//******************************************************************************
+  handleFieldChange = evt => {
+    const stateToChange = {}
+    stateToChange[evt.target.id] = evt.target.value
+    this.setState(stateToChange)
+  }
+
+//******************************************************************************
+//UPDATE existing task
+//******************************************************************************
+updateExistingTask = evt => {
+  evt.preventDefault()
+  this.setState({ loadingStatus: true });
+  const editedTask = {
+    id: this.props.match.params.taskId,
+    userId: loggedInUser,
+    task: this.state.task,
+    expectedCompletionDate: this.state.expectedCompletionDate,
+    isComplete: this.state.isComplete
+  };
+
+  ApiManager.update("tasks", (editedTask)) // API PUT call
+  .then(() => this.props.history.push("/tasks"))  // re-routing
+}
+
+//******************************************************************************
+//componentDidMount()
+//******************************************************************************
+  componentDidMount() {
+    ApiManager.get("tasks", this.props.match.params.taskId)
+      .then(task => {
+        this.setState({
+          task: task.task,
+          expectedCompletionDate: task.expectedCompletionDate,
+          loadingStatus: false,
+        })
+      })}
+
+//******************************************************************************
+//render()
+//******************************************************************************
   render() {
     return (
-      <div>
-        <h1>HELLO EDIT FORM!!!!</h1>
-      </div>
+      <>
+        <form>
+          <fieldset>
+            <div className="card">
+            <label htmlFor="task">Task</label>
+              <input
+                type="text"
+                required
+                className="card-content"
+                onChange={this.handleFieldChange}
+                id="task"
+                value={this.state.task}
+              />
+              <label htmlFor="expectedCompletionDate">Due on</label>
+
+              <input
+                type="date"
+                required
+                className="card-content"
+                onChange={this.handleFieldChange}
+                id="expectedCompletionDate"
+                value={this.state.expectedCompletionDate}
+              />
+            </div>
+
+            <div className="alignRight">
+              <button
+                type="button" disabled={this.state.loadingStatus}
+                onClick={this.updateExistingTask}
+                className="btn btn-primary"
+              >Submit</button>
+            </div>
+          </fieldset>
+        </form>
+      </>
     )
   }
 }
