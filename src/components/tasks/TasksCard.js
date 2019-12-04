78117dafd1844410e345ceb529c15d84a1da7bda
Cherkesky <guy@cherkesky.com>
2019-12-04 14:10:34 -0600

    styling

--- src/components/tasks/TasksCard.js
+++ src/components/tasks/TasksCard.js
@@ -9,7 +9,7 @@ export class TasksCard extends Component {
       <div className="card">
           <div className="card-content">
             <h2 className="card-task">{this.props.task.task}</h2>
-            <p>{convertDateTimeFromISO(this.props.task.expectedCompletionDate).toDateString()}</p>
+            <p>Due on: {convertDateTimeFromISO(this.props.task.expectedCompletionDate).toDateString()}</p>
            
             <div>
             <input type="checkbox" checked={this.props.task.isComplete} onChange={()=> // handle checkbox
