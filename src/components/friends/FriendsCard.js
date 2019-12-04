b140fb0f5c2c87536536dc0b726edbb19f63e1c8
Ryan Bishop <rdbishop06@gmail.com>
2019-12-04 07:35:23 -0600

    refactor: FriendsCard into fxn component

--- src/components/friends/FriendsCard.js
+++ src/components/friends/FriendsCard.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-export class FriendsCard extends Component {
-    render() {
-        const { user, isFriend } = this.props
-        const { removeFriend, addFriend } = this.props
-        
-        return (
-            <div className="card">
-                <div className="card-content">
-                    <p>{user.fullName}</p>
-                    {isFriend ? 
-                        <button className="btn btn-danger" onClick={() => removeFriend(user.deleteId)}>Remove</button> :
-                        <button className="btn btn-success" onClick={() => addFriend(user.id)}>Add Friend</button>
-                    }
+export default function FriendsCard(props) {
+	const { user, isFriend } = props;
+	const { removeFriend, addFriend } = props;
 
-                </div>
-            </div>
-        )
-    }
+	return (
+		<div className="card">
+			<div className="card-body">
+				<h5 className="card-title">{user.fullName}</h5>
+				{isFriend ? (
+					<button className="btn btn-danger" onClick={() => removeFriend(user.deleteId)}>
+						Remove
+					</button>
+				) : (
+					<button className="btn btn-success" onClick={() => addFriend(user.id)}>
+						Add Friend
+					</button>
+				)}
+			</div>
+		</div>
+	);
 }
-
-export default FriendsCard
