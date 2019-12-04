7c22c54c6dc4f1adac523b0832ac81e07c7773dd
Trey Suiter <trey.suiter@gmail.com>
2019-12-04 15:09:28 -0600

    removed delete and edit buttons unless logged in

--- src/components/Articles/ArticlesCard.js
+++ src/components/Articles/ArticlesCard.js
@@ -15,9 +15,11 @@ class ArticleCard extends Component {
                     <p>Synopsis: <span>{this.props.article.synopsis}</span></p>
                     <p>Link: <a href={this.props.article.url} target="_blank" rel="noopener noreferrer">{this.props.article.url}</a></p>
                     <p>Time: <span>{convertDateTimeFromISO(this.props.article.timestamp).toLocaleString()}</span></p>
+                    {this.props.loggedInUser === this.props.article.userId ? <>
                     <button type="button" onClick={() => this.props.deleteArticle(this.props.article.id)}>Delete Article</button>
                     <button type="button"
                         onClick={() => { this.props.history.push(`/articles/${this.props.article.id}/edit`) }}>Edit</button>
+                        </> : null }
                 </div>
             </div>
         );
