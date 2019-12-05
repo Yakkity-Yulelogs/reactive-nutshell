aaa050d2b9fb3849b9b1fa84c287a494aa53a690
Ryan Bishop <rdbishop06@gmail.com>
2019-12-05 13:22:30 -0600

    post new user to API after registration

--- src/components/register/RegisterForm.js
+++ src/components/register/RegisterForm.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
+import ApiManager from '../../modules/ApiManager';
 
 export class RegisterForm extends Component {
   state = {
+    fullName: '',
     email: '',
     password: '',
     passwordA: '',
@@ -27,11 +29,19 @@ export class RegisterForm extends Component {
     e.preventDefault()
     const { passwordA, passwordB } = this.state
     if (passwordA === passwordB && passwordA !== "") {
+      const newUser = {
+        fullName: this.state.fullName,
+        email: this.state.email,
+        password: this.state.passwordA,
+      }
       this.props.setUser({
         email: this.state.email,
         password: this.state.passwordA,
       })
-      this.props.history.push("/")
+      ApiManager.post("users", newUser)
+      .then(() => {
+        this.props.history.push("/")
+      })
     }
     else {
       window.alert("Please make sure your passwords match")
@@ -51,6 +61,7 @@ export class RegisterForm extends Component {
           <div className="card">
             <div className="card-content">
               <form onSubmit={this.handleLogin}>
+                <input type="text" placeholder="Full Name" id="fullName" onChange={this.handleFieldChange} required></input> <br />
                 <input type="text" placeholder="Email" id="email" onChange={this.handleFieldChange} required></input> <br />
                 <input type="password" placeholder="Password" id="passwordA" onChange={this.handleFieldChange} required></input><br />
                 <input type="password" placeholder="Password Again" id="passwordB" onChange={this.handleFieldChange} required></input><br />
